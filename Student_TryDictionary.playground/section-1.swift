2933564930a017143028da6868fbfc8832b5144c
55011212079Miew <imac>
2014-09-03 15:15:34 +0700

    chatchai

---
+++ Student_TryDictionary.playground/section-1.swift
@@ -0,0 +1,34 @@
+// Playground - noun: a place where people can play
+
+class Student {
+    var name: String = "ssss"
+//    var i: String
+    
+    init(name:String) {
+        self.name = name
+    }
+    
+    func returnStudentID() {
+        var studentID = ["Chatchai":54011212057, "Tiwa":54011212088, "Wattarayut":55011212035]
+        
+        for (name, i) in studentID {
+            if name == "Chatchai"{
+                
+                println("Ok")
+                
+            }else{
+                
+                println("No")
+                
+            }
+        }
+    }
+    func addStudent(key:String,value:String){
+    self.name
+    
+    }
+}
+let s = "ss"
+
+let x = Student(name: "Tiwa")
+let y = x.returnStudentID
