a7fa1291cb59625525f59760a21d4fc1350e1a91
ChatchaiBoy <chatchai@Chatchais-MacBook-Pro.local>
2014-09-12 16:55:34 +0700

    chatchai

---
+++ Week5/TipCalculator/TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,50 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by Chatchai on 9/12/2557 BE.
+//  Copyright (c) 2557 Chatchai. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel{
+    //2
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double{
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    //3
+    init(total:Double, taxPct:Double){
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    //4
+    func calcTipWithTipPct(tipPct:Double) -> Double{
+        return subtotal * tipPct
+    }
+    
+    //1
+    func returnPossibleTips() -> [Int: Double]{
+        
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
+        
+        //2
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred{
+            let intPct = Int(possibleTip*100)
+            //3
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+}
+//6
+//let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+//tipCalc.returnPossibleTips()
+
--- Week5/TipCalculator/TipCalculator/ViewController.swift
+++ Week5/TipCalculator/TipCalculator/ViewController.swift
@@ -8,17 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+class ViewController:UIViewController {
+    
+    @IBOutlet var totalTextFile :
+        UITextField!
+    @IBOutlet var taxPctSlider :
+        UISlider!
+    @IBOutlet var taxPctLabel :
+        UILabel!
+    @IBOutlet var resultsTextView :
+        UITextView!
+    
+    let tipCalc = TipCalculatorModel(total:33.25,taxPct: 0.06)
+    
+    @IBAction func calculateTapped(sender : AnyObject){
+        //1
+        tipCalc.total = Double((totalTextFile.text as NSString).doubleValue)
+        //2
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        //3
+        for(tipPct,tipValue) in possibleTips{
+            //4
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        //5
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(sender : AnyObject){
+        tipCalc.taxPct = Double(taxPctSlider.value)/100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(sender : AnyObject){
+        totalTextFile.resignFirstResponder()
+    }
+    
+    
+    func refreshUI(){
+    
+        totalTextFile.text = String(format: "%0.2f" , tipCalc.total)
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
 
 }
